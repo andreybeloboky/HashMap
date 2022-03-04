2e4eff22199ac92450238d3165aa8a4eefa155b1
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-03-04 14:55:40 +0300

    edit main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,23 +7,8 @@ public class Main {
         project.put("KING", 1);
         project.put("BLAKE", 3);
         project.put("Andrew", 213);
-        //System.out.println(project.containsKey("BLAKE"));
-        //System.out.println(project.containsValue(3)); // here is trouble. And the string upper
+        System.out.println(project.containsKey("BLAKE"));
+        System.out.println(project.containsValue(3));
         System.out.println(project.size() + " SIZE");
     }
-
-
-    public static int randomPassportNumber() {
-        int beginPassport = 20000;
-        int endPassport = 100000;
-        return beginPassport + (int) (Math.random() * endPassport);
-    }
-
-    public static int randomData() {
-        int beginData = 0;
-        int endData = 4;
-        return beginData + (int) (Math.random() * endData);
-    }
-
-
 }

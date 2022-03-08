6b0b1b0be1cbd7c9afe1baad18edc2868effbe60
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-03-08 20:34:44 +0300

    add new methods to easy reading the code

--- src/com/company/HashMap1.java
+++ src/com/company/HashMap1.java
@@ -3,6 +3,7 @@ package com.company;
 public class HashMap1 implements Map1 {
     private Node[] array;
     private int countOfElements;
+    private int lock;
 
     public HashMap1() {
         this.array = new Node[16];
@@ -34,29 +35,23 @@ public class HashMap1 implements Map1 {
      */
     @Override
     public boolean containsKey(String key) {
-        int lock = 0;
+        lock = 0;
         int i = 0;
-        boolean str = false;
+        boolean containKey = false;
         while (lock != 1 && i < array.length - 1) {
             Node current = array[i];
             try {
                 while (current.next != null) {
-                    if (current.hash == key.hashCode()) {
-                        str = true;
-                        lock++;
-                    }
+                    containKey = checkContainKey(current, containKey, key);
                     current = current.next;
                 }
-                if (current.hash == key.hashCode()) {
-                    str = true;
-                    lock++;
-                }
+                containKey = checkContainKey(current, containKey, key);
                 i++;
             } catch (NullPointerException e) {
                 i++;
             }
         }
-        return str;
+        return containKey;
     }
 
     /**
@@ -65,54 +60,42 @@ public class HashMap1 implements Map1 {
      */
     @Override
     public boolean containsValue(Object value) {
-        int lock = 0;
+        lock = 0;
         int i = 0;
-        boolean str = false;
+        boolean containValue = false;
         while (lock != 1 && i < array.length - 1) {
             Node current = array[i];
             try {
                 while (current.next != null) {
-                    if (current.value == value) {
-                        str = true;
-                        lock++;
-                    }
+                    containValue = checkContainValue(current, containValue, value);
                     current = current.next;
                 }
-                if (current.value == value) {
-                    str = true;
-                    lock++;
-                }
+                containValue = checkContainValue(current, containValue, value);
                 i++;
             } catch (NullPointerException e) {
                 i++;
             }
         }
-        return str;
+        return containValue;
     }
 
     /**
      * @param key - unique element;
-     * @return -
+     * @return - str (string) who features, whether or not there is data;
      */
     @Override
     public Object get(String key) {
-        int lock = 0;
+        lock = 0;
         int i = 0;
         String str = "This key isn't exist";
         while (lock != 1 && i < array.length - 1) {
             Node current = array[i];
             try {
                 while (current.next != null) {
-                    if (current.hash == key.hashCode()) {
-                        str = current.key + " " + current.value;
-                        lock++;
-                    }
+                    str = checkGet(current, key, str);
                     current = current.next;
                 }
-                if (current.hash == key.hashCode()) {
-                    str = current.key + " " + current.value;
-                    lock++;
-                }
+                str = checkGet(current, key, str);
                 i++;
             } catch (NullPointerException e) {
                 i++;
@@ -128,7 +111,6 @@ public class HashMap1 implements Map1 {
     @Override
     public void put(String key, Object value) {
         int index = key.hashCode() % array.length;
-        System.out.println(index);
         Node newNode = new Node(key, value, key.hashCode());
         Node current = array[index];
         if (array[index] == null) {
@@ -189,10 +171,10 @@ public class HashMap1 implements Map1 {
     }
 
     /**
-     * @param current -
+     * @param current - current Node; LinkedList
      * @param key     - unique element;
      * @param value   - associated element with key;
-     * @param newNode -
+     * @param newNode - new Node; LinkedList
      */
     private void checkLastElement(Node current, String key, Object value, Node newNode) {
         if (current.hash == key.hashCode()) {
@@ -201,5 +183,47 @@ public class HashMap1 implements Map1 {
             current.next = newNode;
             countOfElements++;
         }
+    }
+
+    /**
+     * @param current - current Node;
+     * @param key     - key of data;
+     * @param str     - string default;
+     * @return string, default - if data don't find, key and value - if data is true;
+     */
+    private String checkGet(Node current, String key, String str) {
+        if (current.hash == key.hashCode()) {
+            str = current.key + " " + current.value;
+            lock++;
+        }
+        return str;
+    }
+
+    /**
+     * @param current      - current Node;
+     * @param containValue - false (default);
+     * @param value        - value of data;
+     * @return contain true or false
+     */
+    private boolean checkContainValue(Node current, boolean containValue, Object value) {
+        if (current.value == value) {
+            containValue = true;
+            lock++;
+        }
+        return containValue;
+    }
+
+    /**
+     * @param current    - current Node;
+     * @param containKey - false;
+     * @param key        - key of data;
+     * @return contain true or false
+     */
+    private boolean checkContainKey(Node current, boolean containKey, String key) {
+        if (current.hash == key.hashCode()) {
+            containKey = true;
+            lock++;
+        }
+        return containKey;
     }
 }
